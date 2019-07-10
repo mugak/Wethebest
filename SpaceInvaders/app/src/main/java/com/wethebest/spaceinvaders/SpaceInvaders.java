c17acd3625dec306d3b725e4733894dc5aa54c8f
jtlemkin <jtlemkin@gmail.com>
2019-07-09 18:33:29 -0700

    changed SpaceInvader's to extend Activity class

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/SpaceInvaders.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/SpaceInvaders.java
@@ -1,10 +1,9 @@
 package com.wethebest.spaceinvaders;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class SpaceInvaders extends AppCompatActivity {
+public class SpaceInvaders extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
