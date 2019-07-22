10570d059299ce767548f04f38f66ae7471ee9e9
Muga Kim <ukim@ucdavis.edu>
2019-07-22 15:57:16 -0700

    created AlienArmy class

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/AlienArmy.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/AlienArmy.java
@@ -15,5 +15,4 @@ numAliens
 public class AlienArmy {
 
 
-
 }
---
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/AlienRow.java
@@ -0,0 +1,16 @@
+package com.wethebest.spaceinvaders;
+/*all aliens reverse direction based on when the largest row hits the edge of screen
+as alien count goes down, alien speed increases (exponentially)
+
+AlienRow
+checkBounds
+size
+numAliens*/
+
+public class AlienRow {
+
+
+
+
+
+}
