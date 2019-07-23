b848797ff91c20fdb450ba39082ebf31865fe28c
Sach Samala <sachit478@gmail.com>
2019-07-22 21:48:06 -0700

    updates to Intro class/xml

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/Intro.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/Intro.java
@@ -10,5 +10,9 @@ public class Intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+
+
+
     }
 }
