8ee7f5d8ce14c425183d1046a106db9e6fe54423
Sach Samala <sachit478@gmail.com>
2019-07-22 22:26:17 -0700

    Updates on Intro
    Intro now has a button once player is finished typing his name, he can move onto the game (UI) activity

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/Intro.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/Intro.java
@@ -3,6 +3,19 @@ package com.wethebest.spaceinvaders;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+import android.util.Log;
+
 
 public class Intro extends AppCompatActivity {
 
@@ -12,7 +25,16 @@ public class Intro extends AppCompatActivity {
         setContentView(R.layout.activity_intro);
 
 
+        TextView PlayerName = findViewById(R.id.PlayerName);
+        EditText EnterName = findViewById(R.id.EnterName);
+        Button Enterbtn = findViewById(R.id.Enterbtn);
 
+        Enterbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intro.this, UI.class));
+            }
+        });
 
     }
 }
