2b63c74b9e34418f3ed8cae8571530f6447c0dd5
Muga Kim <ukim@ucdavis.edu>
2019-07-23 17:51:41 -0700

    initialized variables for AlienArmy

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/AlienArmy.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/AlienArmy.java
@@ -7,7 +7,11 @@ import android.os.Bundle;
 import java.util.List;
 
 public class AlienArmy {
-    
+    private int numRows;
+    private Point mScreenSize;
+    private Point rowPosition; //Position of top left corner of first alien in first row
+    private int spaceBetweenRows;
+
     AlienArmy(Point screenSize) {
     }
 
