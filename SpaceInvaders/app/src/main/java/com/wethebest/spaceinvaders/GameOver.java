e1c5574dc4656a40ee91624473c89daba9f5924c
Sach Samala <sachit478@gmail.com>
2019-07-24 22:09:37 -0700

    updates to GameOver Activity
    added scoreboard and changed alignment. Added gameover menu

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/GameOver.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/GameOver.java
@@ -3,6 +3,7 @@ package com.wethebest.spaceinvaders;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class GameOver extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class GameOver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.gameover_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
