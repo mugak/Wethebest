fad505e967fbe63d0882da9df72f50bffcb58a0e
jtlemkin <jtlemkin@gmail.com>
2019-07-24 22:15:24 -0700

    added test for computeBarrierPosition method

--- SpaceInvaders/app/src/test/java/com/wethebest/spaceinvaders/UtilUnitTest.java
+++ SpaceInvaders/app/src/test/java/com/wethebest/spaceinvaders/UtilUnitTest.java
@@ -1,4 +1,17 @@
 package com.wethebest.spaceinvaders;
 
+import android.graphics.Point;
+import android.graphics.PointF;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class UtilUnitTest {
+    @Test
+    public void computeBarrierPosition_isCorrect() {
+        Point screenSize = new Point(10, 10);
+        PointF pos = Util.computeBarrierPosition(1, 1, screenSize);
+        assertEquals(0, pos.x, 0);
+    }
 }
