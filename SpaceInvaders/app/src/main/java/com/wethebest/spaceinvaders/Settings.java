0214ea7aba724ea327691b9ae5e8e17896be8cef
Sach Samala <sachit478@gmail.com>
2019-07-25 21:44:45 -0700

    Settings: Volume
    added increase volume function

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/Settings.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/Settings.java
@@ -17,8 +17,20 @@ public class Settings extends AppCompatActivity {
         Button lower_volume = findViewById(R.id.Lower_Volume);
         Button Higher_Volume = findViewById(R.id.higher_volume);
 
-        
+        AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
 
+        //To increase volume
+        Higher_Volume.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
+            }
+        });
+
+        
 
 
     }
