e1f064e9f69c19c342bf848980c19e06e9351a2b
jtlemkin <jtlemkin@gmail.com>
2019-07-26 15:18:35 -0700

    added changestate method to wave state

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/WaveState.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/WaveState.java
@@ -1,9 +1,18 @@
 package com.wethebest.spaceinvaders;
 
+/*
+    This state represents the core game play where the player shoots down an advancing wave of
+    aliens
+ */
 public class WaveState implements GameState {
     @Override
     public void changeState(SpaceInvadersApp app, State nextState) {
-
+        switch(nextState) {
+            case PAUSE:
+                app.setState(new PauseState());
+            case GAMEOVER:
+                app.setState(new GameOverState());
+        }
     }
 
     @Override
