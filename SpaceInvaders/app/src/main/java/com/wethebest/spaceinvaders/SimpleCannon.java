9d692284b28d6cf87075edc3c598581e20b7d86c
Jeffrey Chen <jefche@ucdavis.edu>
2019-07-29 18:32:07 -0700

    Documentation in PlayerProj, AlienProj, and SimpleCannon

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/SimpleCannon.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/SimpleCannon.java
@@ -6,6 +6,7 @@ import android.graphics.PointF;
 * This game object shoots projectiles and is controlled by the player.
 * Movement is horizontally left or right and is determined by the device's accelerometer sensor.
 * When the player shoots, a PlayerProj object at the position of the cannon.
+* The cannon starts with MAX_LIVES number of lives and has a short interval of invincibility when hit.
 */
 public class SimpleCannon extends GameObject {
     //DEFAULTS
@@ -22,13 +23,19 @@ public class SimpleCannon extends GameObject {
     private long frameCount = 0;
 
     SimpleCannon(SpaceInvadersApp app) {
-        super(app, new PointF(app.mScreenSize.x / 10, app.mScreenSize.x / 10), R.drawable.player, new PointF(app.mScreenSize.x / 2, app.mScreenSize.y - app.mScreenSize.x/10));
+        super(app,
+                new PointF(app.mScreenSize.x / 10, app.mScreenSize.x / 10),
+                R.drawable.player,
+                new PointF(app.mScreenSize.x / 2, app.mScreenSize.y - app.mScreenSize.x/10));
         //super(app, size, sprite, position)
     }
 
     public void update(long fps) {
-        if(((SpaceInvaders)app.context).yAcceleration >= .08f || ((SpaceInvaders)app.context).yAcceleration <= -.08f) { //tilt thresholds for cannon to stay still
-            mHitBox.moveHorizontally(((SpaceInvaders)app.context).yAcceleration * 10); //change this multiplying constant to change movement speed
+        if(((SpaceInvaders)app.context).yAcceleration >= .08f //tilt thresholds for cannon to stay still
+                || ((SpaceInvaders)app.context).yAcceleration <= -.08f)
+        {
+            //change this multiplying constant to change movement speed
+            mHitBox.moveHorizontally(((SpaceInvaders)app.context).yAcceleration * 10);
             mHitBox.horizontalStayInBounds();
         }
 
