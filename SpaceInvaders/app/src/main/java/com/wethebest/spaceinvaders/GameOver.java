88818d69b8505da5c6142db9484fbee1ea4df3c3
Muga Kim <ukim@ucdavis.edu>
2019-08-02 02:48:29 -0700

    updated UI activities
    i know, D.R.Y.

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/GameOver.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/GameOver.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,6 +21,8 @@ public class GameOver extends Activity {// extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        enableImmersiveMode();
 
         TextView Score = findViewById(R.id.Score);
         Score.setText(String.valueOf(SpaceInvadersApp.score));
@@ -51,6 +54,31 @@ public class GameOver extends Activity {// extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if(hasFocus) {
+            enableImmersiveMode();
+        }
+    }
+
+    protected void enableImmersiveMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
+
+
+    } //enables full screen - took from https://stackoverflow.com/questions/29186081/android-immersive-mode-reset-when-changing-activity
+
 
 //    //menu
 //    @Override
