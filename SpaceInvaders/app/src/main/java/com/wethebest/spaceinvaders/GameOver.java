5b7ddcc5f964ced61bdca5f29dcdbf587ba5c8a5
Sach Samala <sachit478@gmail.com>
2019-08-01 22:51:36 -0700

    Update GameOver.java

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/GameOver.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/GameOver.java
@@ -2,8 +2,11 @@ package com.wethebest.spaceinvaders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 /*
 GameOver draws the game over screen
  */
@@ -16,7 +19,17 @@ public class GameOver extends AppCompatActivity {
         setContentView(R.layout.activity_game_over);
 
 
-        
+        Button StartOver = findViewById(R.id.StartOver_btn);
+        StartOver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(GameOver.this,Intro.class);
+                startActivityForResult(intent,0);
+                overridePendingTransition( R.anim.trans_left_in, R.anim.trans_left_out );
+
+            }
+        });
     }
 
 }
