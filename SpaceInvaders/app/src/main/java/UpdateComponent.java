bfc109f8a42012239325f9ea89e7e102c2d21b41
Idan Shemy <ishemy@ucdavis.edu>
2019-08-02 02:46:55 -0700

    addded updatecomponent interface

---
+++ SpaceInvaders/app/src/main/java/UpdateComponent.java
@@ -0,0 +1,8 @@
+package com.wethebest.spaceinvaders;
+
+// starter code source: “Learning Java by Building Android Games - Second Edition.”
+
+
+public interface UpdateComponent {
+    void update(long fps, Transform t, Transform playerTransform);
+}
