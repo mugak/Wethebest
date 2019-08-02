cd1a69d6b7a25bb0ba7e70e5a6f7313a2eec825c
Idan Shemy <ishemy@ucdavis.edu>
2019-08-02 02:55:22 -0700

    added methods to transform class

--- SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/Transform.java
+++ SpaceInvaders/app/src/main/java/com/wethebest/spaceinvaders/Transform.java
@@ -32,4 +32,93 @@ public class Transform {
         mStartingPosition = new PointF(
                 mLocation.x, mLocation.y);
     }
+
+    public void updateCollider() {
+        mCollider.top = mLocation.y;
+        mCollider.left = mLocation.x;
+        mCollider.bottom =
+                (mCollider.top + mObjectHeight);
+
+        mCollider.right =
+                (mCollider.left + mObjectWidth);
+    }
+
+
+    public RectF getCollider() {
+        return mCollider;
+    }
+
+    void headUp() {
+        mHeadingUp = true;
+        mHeadingDown = false;
+    }
+
+    void headDown() {
+        mHeadingDown = true;
+        mHeadingUp = false;
+    }
+
+    boolean headingUp() {
+        return mHeadingUp;
+    }
+
+    boolean headingDown() {
+        return mHeadingDown;
+    }
+
+    float getSpeed() {
+        return mSpeed;
+    }
+
+    PointF getLocation() {
+        return mLocation;
+    }
+
+    PointF getSize() {
+        return new PointF(
+                (int) mObjectWidth,
+                (int) mObjectHeight);
+    }
+
+    void headRight() {
+        mHeadingRight = true;
+        mHeadingLeft = false;
+        mFacingRight = true;
+
+    }
+
+    void headLeft() {
+        mHeadingLeft = true;
+        mHeadingRight = false;
+        mFacingRight = false;
+    }
+
+    boolean headingRight() {
+        return mHeadingRight;
+    }
+
+    boolean headingLeft() {
+        return mHeadingLeft;
+    }
+
+    void stopHorizontal() {
+        mHeadingLeft = false;
+        mHeadingRight = false;
+    }
+
+    void stopMovingLeft() {
+        mHeadingLeft = false;
+    }
+
+    void stopMovingRight() {
+        mHeadingRight = false;
+    }
+
+    boolean getFacingRight() {
+        return mFacingRight;
+    }
+
+    PointF getStartingPosition() {
+        return mStartingPosition;
+    }
 }
